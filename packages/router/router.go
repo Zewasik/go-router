8380c408b7e855bf16e3622fc3a87233f6d1bc2f
Zewas <shumahershik@gmail.com>
2023-02-07 21:29:04 +0200

    add serve and getField functions add comments to all functions

--- packages/router/router.go
+++ packages/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"context"
 	"net/http"
 	"regexp"
 )
@@ -15,7 +16,10 @@ type Router struct {
 	routes []route
 }
 
-// creating new route and appending it to router
+// NewRouter creates new route and appends it to Router,
+// method specifies the method that is allowed,
+// regexp must contain a named group,
+// parsed value will be accessable through handler's context via GetField function
 //
 // regexp example -> https://regex101.com/r/84S9iL/1
 func (router *Router) NewRoute(method, regexpString string, handler http.HandlerFunc) {
@@ -25,4 +29,37 @@ func (router *Router) NewRoute(method, regexpString string, handler http.Handler
 		regex,
 		handler,
 	})
+}
+
+// If there is a exact match Serve redirects to the associated handler function.
+//
+// When matched, regular expression groups are used as key value pairs
+// accessible in handler's context via GetField function
+func (router *Router) Serve(w http.ResponseWriter, r *http.Request) {
+	for _, v := range router.routes {
+		match := v.regex.FindStringSubmatch(r.URL.Path)
+		if len(match) > 0 {
+			if r.Method != v.method {
+				w.WriteHeader(http.StatusMethodNotAllowed)
+				return
+			}
+			matchMap := make(map[string]string)
+			groupName := v.regex.SubexpNames()
+
+			// map group name(key) to submatched result
+			// these arrays have one to one relationship
+			for i := 1; i < len(match); i++ {
+				matchMap[groupName[i]] = match[i]
+			}
+			ctx := context.WithValue(r.Context(), struct{}{}, matchMap)
+			v.handler(w, r.WithContext(ctx))
+			return
+		}
+	}
+}
+
+// Gets key value pairs from matched URL variables
+func GetField(r *http.Request, name string) string {
+	fields := r.Context().Value(struct{}{}).(map[string]string)
+	return fields[name]
 }
