d014dcfec1a1bf6c947f847ee71a10836f6739f6
Zewas <shumahershik@gmail.com>
2023-02-09 14:36:07 +0200

    add error handling in GetField function move controllers folder to root

--- controlles/controller.go
+++ controlles/controller.go
@@ -2,11 +2,17 @@ package controllers
 
 import (
 	"go-router/packages/httpRouting"
+	"log"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	id := httpRouting.GetField(r, "id")
+	id, err := httpRouting.GetField(r, "id")
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
 	w.Write([]byte("Home page, id: " + id))
 }
