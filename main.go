5fd6eecff51b9a2bc4a4ae31b0aa1103b414e995
Zewas <shumahershik@gmail.com>
2023-02-09 22:02:14 +0200

    fix folder name finish issue

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"go-router/packages/controllers"
+	"go-router/controllers"
 	"go-router/packages/httpRouting"
 	"log"
 	"net/http"
@@ -14,7 +14,6 @@ func main() {
 	r.NewRoute("GET", `.*`, controllers.NotFound)
 
 	http.HandleFunc("/", r.Serve)
-	http.Handle("/src/", http.StripPrefix("/src/", http.FileServer(http.Dir("./src/"))))
 
 	log.Println("Ctrl + Click on the link: http://localhost:8080")
 	log.Println("To stop the server press `Ctrl + C`")
